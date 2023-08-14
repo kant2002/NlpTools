6a430e2c533e41ec8f1905512a9e31c338e58f08
Andrii Kurdiumov <kant2002@gmail.com>
2023-08-14 13:40:22 +0600

    Add support for larger numbers

--- NlpTools.Tests/Numbers2WordsTests.fs
+++ NlpTools.Tests/Numbers2WordsTests.fs
@@ -8,7 +8,7 @@ open System.Linq
 let values : obj[] seq = 
     let parseLine (line: string) =
         let parts = line.Split [| '|' |]
-        let num = parts[0]|> int
+        let num = parts[0]|> int64
         let gender = 
             match parts[1] with
             | "masculine" -> Masculine
@@ -18,12 +18,16 @@ let values : obj[] seq =
         let case = parts[3]
         let expected = parts[4]
         [| num :> obj ; form; gender; case; expected |]
+    let fit_int64 (line: string)  =
+        let parts = line.Split [| '|' |]
+        parts[0].Length <= 21
     let lines =
         File.ReadAllLines("uk.csv").Skip(1)
+        |> Seq.filter fit_int64
         |> Seq.map parseLine
     lines
 
 [<Theory>]
 [<MemberData("values")>]
-let ``Ukrainian`` (number: int, form: string, gender: Gender, case: string, expected: string) =
+let ``Ukrainian`` (number: int64, form: string, gender: Gender, case: string, expected: string) =
     Assert.Equal(expected, (toText "uk" number form gender case))
--- NlpTools/Numbers.fs
+++ NlpTools/Numbers.fs
@@ -57,38 +57,79 @@ module Number2Words =
         "дев'ятсот"
     |]
 
+    let large_numbers = [|
+        [| "тисяча"; "тисячи"; "тисяч" |];
+        [| "мільйон"; "мільйона"; "мільйонів" |];
+        [| "мільярд"; "мільярда"; "мільярдів" |];
+        [| "трильйон"; "трильйона"; "трильйонів" |];
+        [| "квадрильйон"; "квадрильйона"; "квадрильйонів" |];
+        [| "квінтильйон"; "квінтильйона"; "квінтильйонів" |];
+        [| "секстильйон"; "секстильйона"; "секстильйонів" |];
+        [| "септильйон"; "септильйона"; "септильйонів" |];
+        [| "октильйон"; "октильйона"; "октильйонів" |];
+        [| "нонільйон"; "нонільйона"; "нонільйонів" |];
+    |]
+
     type Gender =
     | Feminine
     | Masculine
 
-    let toText lang number form gender case =
+    let private numberForm number =
+        let target = number % 100
+        if target = 1 then
+            0
+        elif target <= 4 then
+            1
+        else
+            2
+
+    let private printThousands (result: StringBuilder) number gender =
+        let hundreds = (number / 100) % 10
+        let tens = (number / 10) % 10
+        let ones = number % 10
+        if hundreds > 0 then
+            result.Append(hundreds_text[hundreds - 1]) |> ignore
+            if tens > 0 || ones > 0 then
+                result.Append(" ") |> ignore
+        if tens >= 2 then
+            result.Append(tens_text[tens - 2]) |> ignore
+            if ones > 0 then
+                result.Append(" ") |> ignore
+        elif tens = 1 then
+            result.Append(larger[tens * 10 + ones - 3]) |> ignore
+
+        if ones > 0 && tens <> 1 then
+            if ones <= 2 then
+                let small_num = 
+                    match gender with
+                    | Masculine -> small[0]
+                    | Feminine -> small[1]
+                result.Append(small_num[ones - 1]) |> ignore
+            else
+                result.Append(larger[ones - 3]) |> ignore
+
+    let toText lang (number: int64) form gender case =
         if lang <> "uk" then failwith "Only Ukrainian supported"
         if number = 0 
             then "нуль"
         else
-            let hundreds = (number / 100) % 10
-            let tens = (number / 10) % 10
-            let ones = number % 10
             let result = StringBuilder()
-            if hundreds > 0 then
-                result.Append(hundreds_text[hundreds - 1]) |> ignore
-                if tens > 0 || ones > 0 then
-                    result.Append(" ") |> ignore
-            if tens >= 2 then
-                result.Append(tens_text[tens - 2]) |> ignore
-                if ones > 0 then
-                    result.Append(" ") |> ignore
-            elif tens = 1 then
-                result.Append(larger[tens * 10 + ones - 3]) |> ignore
-
-            if ones > 0 && tens <> 1 then
-                if ones <= 2 then
-                    let small_num = 
-                        match gender with
-                        | Masculine -> small[0]
-                        | Feminine -> small[1]
-                    result.Append(small_num[ones - 1]) |> ignore
+            let rec x depth num g =
+                let reminder = (num % 1000L) |> int
+                let basic = num / 1000L
+                if (basic = 0) then
+                    printThousands result reminder g
+                    if depth > 0 then
+                        result.Append(" ") |> ignore
+                        let form = numberForm reminder
+                        result.Append(large_numbers[depth - 1][form]) |> ignore
                 else
-                    result.Append(larger[ones - 3]) |> ignore
-
+                    x (depth + 1) basic (if depth = 0 then Feminine else Masculine)
+                    if reminder <> 0 then
+                        result.Append(" ") |> ignore
+                        x depth reminder g
+                
+            x 0 number gender
             result.ToString()
+    //let toText lang (number: int32) form gender case =
+    //    toText lang (number |> int64) form gender case
